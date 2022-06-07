5169e824461f3f59022bab34fba60e1209e8454d
ticiano20 <barabinoticiano@gmail.com>
2022-06-07 18:50:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>MARKET</h1>
+    <footer>Marco Ticiano Barabino-INC-2022</footer>
+    </div>);
+}
+
+export default App;
